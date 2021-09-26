05727ef3fee80b1f8258f4f915951579f57f1836
Vitor Bruno de Oliveira Barth <vitor.barth@gmail.com>
2021-09-25 21:14:01 -0400

    Initial unit tests

--- Sources/SwiftMI/SwiftMI.swift
+++ Sources/SwiftMI/SwiftMI.swift
@@ -1,6 +1,24 @@
 public struct SwiftMI {
-    public private(set) var text = "Hello, World!"
-
     public init() {
+        
+    }
+    
+    public func getClasses(data: [Int]) -> [Int: Int] {
+        return [0: 4,
+                1: 2,
+                2: 1,
+                3: 1]
+    }
+    
+    public func getProbabilities(classes: [Int: Int]) -> [Float] {
+        return [1/2,
+                1/4,
+                1/8,
+                1/8]
+    }
+    
+    public func entropy(data: [Int], base: Int = 2) -> Float {
+        return 7/4
     }
 }
+
--- Tests/SwiftMITests/SwiftMITests.swift
+++ Tests/SwiftMITests/SwiftMITests.swift
@@ -2,10 +2,57 @@ import XCTest
 @testable import SwiftMI
 
 final class SwiftMITests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftMI().text, "Hello, World!")
+    private var data = [0, 1, 3, 0, 2, 1, 0, 0]
+    
+    private var MI = SwiftMI()
+    
+    private var expectedClasses = [0: 4, 1: 2, 2: 1, 3: 1]
+    private var expectedCount = 8
+    
+    private var expectedProbabilities: [Float] = [1/2, 1/4, 1/8, 1/8]
+    private var expectedEntropy: Float = 7/4
+    
+    func testGetClasses() throws {
+        XCTAssertEqual(
+            MI.getClasses(data: data),
+            self.expectedClasses,
+            "Classes counted incorrectly"
+        )
+    }
+    
+    func testGetClassesTotals() throws {
+        let classes = MI.getClasses(data: data)
+    
+        XCTAssertEqual(
+            classes.values.reduce(0) { total, i in return total + i },
+            expectedCount,
+            "Number of elements counted should be equal the size of the input array"
+        )
+    }
+    
+    func testGetProbabilities() throws {
+        XCTAssertEqual(
+            MI.getProbabilities(classes: expectedClasses),
+            expectedProbabilities,
+            "Probabilities calculated incorrectly"
+        )
+    }
+    
+    func testGetProbabilitiesTotals() throws {
+        let probabilities = MI.getProbabilities(classes: expectedClasses)
+    
+        XCTAssertEqual(
+            probabilities.reduce(0) { total, i in return total + i },
+            1,
+            "The sum of the probabilities of the classes should be equal to 1"
+        )
+    }
+    
+    func testGetEntropy() throws {
+        XCTAssertEqual(
+            MI.entropy(data: data),
+            expectedEntropy,
+            "Entropy calculated incorrectly"
+        )
     }
 }
