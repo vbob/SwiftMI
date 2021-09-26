615382a8d2182b4deaf202dc933b74ff4ed170c9
Vitor Bruno de Oliveira Barth <vitor.barth@gmail.com>
2021-09-25 21:40:45 -0400

    Implemented basic functions

--- Sources/SwiftMI/SwiftMI.swift
+++ Sources/SwiftMI/SwiftMI.swift
@@ -1,24 +1,39 @@
+import Foundation
+
 public struct SwiftMI {
-    public init() {
-        
-    }
+    public init() {}
     
+    private func logC(val: Double, forBase base: Double) -> Double {
+        return log(val)/log(base)
+    }
+
     public func getClasses(data: [Int]) -> [Int: Int] {
-        return [0: 4,
-                1: 2,
-                2: 1,
-                3: 1]
+        var classes: [Int: Int] = [:]
+        
+        for value in data {
+            if classes[value] == nil {
+                classes[value] = 1
+            } else {
+                classes[value]! += 1
+            }
+        }
+        
+        return classes
     }
     
     public func getProbabilities(classes: [Int: Int]) -> [Float] {
-        return [1/2,
-                1/4,
-                1/8,
-                1/8]
+        let total: Float = Float(classes.values.reduce(0) { total, x in return total + x })
+        let sortedKeys = classes.keys.sorted()
+        
+        return sortedKeys.map { x in return Float(Float(classes[x]!) / total)}
     }
     
     public func entropy(data: [Int], base: Int = 2) -> Float {
-        return 7/4
+        let classes = getClasses(data: data)
+        let probabilities = getProbabilities(classes: classes)
+        
+        return probabilities.reduce(0) { total, p in
+            return total - (p * Float(logC(val: Double(p), forBase: Double(base))))
+        }
     }
 }
-
